a02772a6b1aa4bcaf8c267f8d9842a9014c1678b
Abhishek Saha <abhisheksaha11@gmail.com>
2017-01-10 16:15:26 +0100

    Update prod.php

--- config/prod.php
+++ config/prod.php
@@ -3,8 +3,8 @@ $app['twig.path'] = array(__DIR__ . '/../src/templates');
 $app['twig.options'] = array('cache' => __DIR__ . '/../var/cache/twig');
 $app['db.options'] = array(
     'dsn'      => 'mysql:host=localhost;dbname=NewABTest',
-    'username' => 'root',
-    'password' => 'rock4me',
+    'username' => '',
+    'password' => '',
     'frozen'   => true
 );
 require __DIR__ . '/security.php';
